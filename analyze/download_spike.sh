8e75b2df5b4f2582b528d7e5ee8b12ed0a55489b
Kosuke Arase <k.arase@conol.jp>
2016-11-10 15:40:40 +0900

    add ./analyze/ directory and delete some unnecessary files in ./src/

---
+++ analyze/download_spike.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# usage
+# sh download_spike.sh [jobID] [local path after $HOME]
+# ex. sh download_spike.sh 5475341 lab/result/compare_compartments/
+
+source ~/.bashrc
+
+target="$2/$1"
+echo $target
+echo $HOME
+mkdir $HOME/$target
+scpfromk result/spike/$1/* $HOME/$target
--- src/singlecmpt_test/parallel.sh
+++
@@ -1,6 +0,0 @@
-
-#! /bin/sh
-
-mpiexec -np 8 ../../specials/x86_64/special -mpi parallel_gaba_test.hoc
-#../../specials/x86_64/special parallel_gaba_test.hoc
-#mpiexec -np 2 python parallel_gap_sample.py
