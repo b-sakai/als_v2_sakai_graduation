8120fbf4cfcb2927107c6713f1be514916e6b772
KosukeArase <k.ara.0605.19107@gmail.com>
2017-01-15 19:06:28 +0900

    make LN - PN synapse lists

--- input/synapse_list/detect_synapses.py
+++ input/synapse_list/detect_synapses.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
 """
-LN - LN 結合を求めるプログラム
-synapse 間距離 < sys.argv[1] を満たす組を抽出し，synapses_between_LN.dat として保存するプログラム
+シナプスを求めるプログラム
+synapse 間距離 < sys.argv[1] を満たす組を抽出し，synapses_between_300_301.dat などとして保存するプログラム
+swc ファイルの指定と出力ファイルの指定は随時書き換えてください．
 
 Usage:
-$ python detect_synapses_between_LN.py
+$ python detect_synapses.py
 
-by Kosuke Arase 20170113
+by Kosuke Arase 20170115
 """
 
 import sys
@@ -43,18 +44,22 @@ def calc_distance():
 if __name__ == "__main__":
     max_distance = float(sys.argv[1])
 
-    swc_path1 = "../swc/040823_5_sn_bestrigid0106_mkRegion.swc"
-    swc_path2 = "../swc/050205_7_sn_bestrigid0106_mkRegion.swc"
-    output_filename = "synapses_between_LN.dat"
+    swc_path_200 = "../swc/050622_4_sn_bestrigid0106_mkRegion.swc"
+    swc_path_300 = "../swc/040823_5_sn_bestrigid0106_mkRegion.swc"
+    swc_path_301 = "../swc/050205_7_sn_bestrigid0106_mkRegion.swc"
 
-    comps1 = read_swc(swc_path1)
-    comps2 = read_swc(swc_path2)
+    # output_filename = "synapses_between_200_300.dat"
+    output_filename = "synapses_between_200_300.dat"
+    # output_filename = "synapses_between_300_301.dat"
+
+    comps1 = read_swc(swc_path_200)
+    comps2 = read_swc(swc_path_300)
 
     synlist = []
     calc_distance()
 
     with open(output_filename, "w") as f:
-        f.write("# 300_syn 301_syn\n")
+        f.write("# %s_syn %s_syn\n" % (output_filename.split(".")[0].split("_")[2], output_filename.split(".")[0].split("_")[3]))
         f.write("# num of data: %d\n" % len(synlist))
         for syn in synlist:
             f.write("%d %d %f\n"%syn)
