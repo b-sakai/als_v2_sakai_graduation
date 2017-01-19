7c7e5dd9898f6b33edbad667837f3b81f1c588c1
KosukeArase <k.ara.0605.19107@gmail.com>
2017-01-20 02:58:59 +0900

    modified connection to distinguish toroid and the others

--- input/synapse_list/make_synapse.py
+++ input/synapse_list/make_synapse.py
@@ -53,23 +53,38 @@ def make_synapse_Arase():
             gid_index = int(post_cell[0])-2
             #k = 4 if gid_index == 0 else 1
             if gid_index == 0: # LN to PN
-                k = 1 # 4
-                n = 10
-            else: # LN to LN
-                k = 1
+                n = 20
+                n_toroid = 0
+            else: # LN to LN in og
                 n = 300
+                n_toroid = 20
 
 
             f.write("$ PRE_CELL %s\n" % pre_cell)
             f.write("$ POST_CELL %s\n" % post_cell)
-            f.write("$ NCONNECTIONS %d\n" % (k*n))
+            f.write("$ NCONNECTIONS %d\n" % (n + n_toroid))
 
-            index = random.sample(xrange(len(synlist[0])), k*n)
+            index = random.sample(xrange(len(synlist[0])), n)
 
-
+            """ ln - pn in toroid or ln - ln in OG """
             for i in index:
                 gid[gid_index] += 1
                 f.write("%d %d %d\n" % (synlist[1][i], synlist[0][i], gid[gid_index]))
+
+
+            """ ln - ln in toroid """
+            if post_cell[0] == "3":
+                if pre_cell[2] == "0":
+                    synlist = [syn_300_301_toroid[1], syn_300_301_toroid[0]]
+                elif pre_cell[2] == "1":
+                    synlist = syn_300_301_toroid
+                else:
+                    print "something wrong with %s" % file
+                index = random.sample(xrange(len(synlist[0])), n_toroid)
+                for i in index:
+                    gid[gid_index] += 1
+                    f.write("%d %d %d\n" % (synlist[1][i], synlist[0][i], gid[gid_index]))
+
 
 
 if __name__ == "__main__":
@@ -79,7 +94,8 @@ if __name__ == "__main__":
 
     syn_200_300 = read_data("synapses_between_200_300.dat")
     syn_200_301 = read_data("synapses_between_200_301.dat")
-    syn_300_301 = read_data("synapses_between_LN.dat")
+    syn_300_301 = read_data("synapses_between_LN_OG.dat")
+    syn_300_301_toroid = read_data("synapses_between_LN_toroid.dat")
 
     # nconnections = 10
 
